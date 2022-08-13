24a54b888df938ad573cc1f561da2a9d7e0c297d
qntjrrud11 <71332005+qntjrrud11@users.noreply.github.com>
2022-08-13 17:13:46 +0900

    [ 22.08.13 17:00 ] Succes

--- Expressed_as_N.py
+++ Expressed_as_N.py
@@ -1,14 +1,23 @@
 def solution(N, number):
-    number_set = []
-    number_set.append({N})
+    number_set = {1:set([int(N)])}
+    
     for n in range(2,9):
         if N == number:
             return 1
-        for i in range(n):
-            for x in list(number_set[i]):
-            
+        number_set[n] = set([int(str(N)*n)])
+        
+        for i in range(1,n):
+            for x in number_set[i]:
+                for y in number_set[n-i]:
+                    number_set[n].add(x+y)
+                    number_set[n].add(abs(x-y))
+                    number_set[n].add(x*y)
+                    if y != 0:
+                        number_set[n].add(x//y)
+                    if number in number_set[n]:
+                        return n
 
+    return -1
 
-    return answer
-
-solution(5,12)
+answer = solution(5,12)
+print(answer)
