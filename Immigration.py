e2592aa1d68a092e6af07fb2d503cbe60db9a73d
qntjrrud11 <71332005+qntjrrud11@users.noreply.github.com>
2022-08-16 22:36:19 +0900

    [22.08.16 22:00] fail Time out

--- Immigration.py
+++ Immigration.py
@@ -1,15 +1,39 @@
 import math
 def solution(n,time):
+
     x = 0
     for i in time:
-        x += 1/i
+        x += 1/i   
     near_time = n/x
-    answer_list = []
-    for i in time:
-        if near_time%i == 0:
-            answer_list.append(int(i*(near_time//i)))
-        else:
-            answer_list.append(int(i*(near_time//i)+i))
+    n_t = int(near_time)
 
-    return min(answer_list)
-print(solution(2, [10, 10,3,5,6,2]))
+    while 1:
+        human = 0
+        for t in time:
+            human += n_t//t
+        if human >= n:
+            return n_t
+        else:
+            n_t += 1
+        print(n_t-1, human)
+
+    
+    # answer_list = {}
+    # for j in time:
+        
+    #     if near_time%j == 0:
+    #         answer_list[j] = int(j*(near_time//j))
+    #     else:
+    #         answer_list[j] = int(j*(near_time//j)+j)
+
+    # ans_val = list(answer_list.values())
+    # ans_keys = list(answer_list.keys())
+    # for k in sorted(zip(ans_val,ans_keys)):
+    #     n -= (k[0]//k[1])
+    #     if n < 0:
+    #         answer_list[k[1]] = k[0]-k[1]
+    #         n = 0
+    # return max(answer_list.values())
+print(solution(10, [1,1,1,1,2]))
+
+    
